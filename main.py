ec8788844ed09de61b246191cec2df3c29cc6ee6
Matheus Borges <matheus.borgest@gmail.com>
2022-05-10 15:31:29 +0100

    comentário no código e pequenas mudanças

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from colorama import Fore, Style
 class DataFrame:
 
     def __init__(self, file_path):
+        """
+        Inicializa a classe com os dados do arquivo .csv e também variávels
+        com metadados utilizados nas funções da classe
+
+        Args:
+            file_path (string): caminho até o arquivo .csv
+        """
         assert file_path is not None, 'Please inform the file path'
         self.__read_csv(file_path)
         
@@ -20,15 +27,25 @@ class DataFrame:
         self.factorized_cols = dict()
 
     def __getitem__(self, key):
+        """
+        Implementa diferentes maneiras de recuperar os valores do dataset
+        de acordo com o tipo da chave passada como parâmetro.
+        """
         key_type = type(key)
+        
+        # Retorna uma linha do dataset
         if key_type == int:
             return self.values[key]
+        
+        # Retorna a coluna referente à string informada.
         elif key_type == str:
             j = self.__get_col_index_by_key(key)
             return np.array(
                 [self.values[i][j] for i in range(self.shape[0])],
                 dtype=self.column_types[j]
             )
+            
+        # Returna as colunas referentes ao intervalo de índices da tupla
         elif key_type == tuple:
             assert len(key) == 2, 'Invalid tuple size'
             if key[1] < 0:
@@ -41,11 +58,18 @@ class DataFrame:
             raise Exception('Invalid key type')
 
     def __init_col_to_index_dict(self):
+        """
+        Cria um dicionário do tipo nome da coluna -> índice
+        """
         self.colum_to_index_dict = dict()
         for i, c in enumerate(self.columns):
             self.colum_to_index_dict[c] = i
 
     def __init_col_types(self):
+        """
+        Cria um vetor referente ao tipos de dados em cada coluna e aplica
+        uma conversão do tipo aos elementosç
+        """
         def get_value_type(v):
             try:
                 int(v)
@@ -69,13 +93,21 @@ class DataFrame:
                 self.values[i][j] = type(self.values[i][j])
 
     def __read_csv(self, file_path):
+        """
+        Função de leitura do arquivo e armazenamento em variáveis.
+        """
         with open(file_path, 'r') as f:
             lines = f.readlines()
         
         self.columns = lines[0].replace('\n', '').split(',')
+        assert len(self.columns) == len(set(self.columns)), 'Duplicated column names'
+        
         self.values = [l.replace('\n', '').split(',') for l in lines[1:]]
 
     def __get_col_index_by_key(self, key):
+        """
+        Retorna o índice da coluna referente a uma chave
+        """
         key_type = type(key)
         if key_type == str:
             assert key in self.columns, 'Invalid key'
@@ -86,6 +118,10 @@ class DataFrame:
             raise Exception('Invalid key type')
 
     def __continuous_split(self, key):
+        """
+        Transforma uma coluna de valores contínuos (float) em uma de valores
+        categóricos, divididos entre maiores e menores ou iguais ao valor mediano dos dados
+        """
         col_i = self.__get_col_index_by_key(key)
         assert self.column_types[col_i] == float, 'This can only be done to continuous valued columns'
 
@@ -98,11 +134,19 @@ class DataFrame:
         self.column_types[col_i] = str
 
     def categorize_continuous_values(self):
-        for i, t in enumerate(self.column_types):
+        """
+        Aplica a função de categorização a todas as colunas do tipo float
+        """
+        for j, t in enumerate(self.column_types):
             if t == float:
-                self.__continuous_split(i)
+                self.__continuous_split(j)
 
     def factorize(self, key):
+        """
+        Transforma os valores de uma coluna em uma "enumeração" dos mesmos
+        e cria um dict para se obter os valores originais a partir dos
+        valores fatorados.
+        """
         col_i = self.__get_col_index_by_key(key)
 
         classes = set(self[self.columns[col_i]])
@@ -118,6 +162,10 @@ class DataFrame:
         self.column_types[col_i] = int
 
     def train_test_split(self, X_indexes, y_class=-1, perc=.25):
+        """
+        Divide o dataset em um de treino e outro de teste, além de separar
+        os dados entre features e labels
+        """
         split_index = int(self.shape[0] * (1 - perc))
         shuffled_data = deepcopy(self.values)
         random.shuffle(shuffled_data)
@@ -133,17 +181,21 @@ class DataFrame:
         return X_train, X_test, y_train, y_test
 
     def get_values_by_cols(self, cols):
+        """
+        Retorna o dataset com somenete as colunas desejadas.
+        """
         return [[a for i, a in enumerate(x) if i in cols] for x in self.values]
     
 
 class Node:
 
     def __init__(self, X, y, col_names, depth=0, parent=None):
-        assert len(X) == len(y)
+        """
+        Inicializa o nó individual da decision tree.
+        """
+        assert len(X) == len(y), 'Mismatch of features and labels size'
         if parent is None:
             assert len(X) > 0 and len(y) > 0
-        #if len(X) > 0:
-            #assert len(X[0]) == len(col_names)
 
         self.X, self.y = deepcopy(X), deepcopy(y)
         self.col_names = deepcopy(col_names)
@@ -156,22 +208,35 @@ class Node:
             self.__generate_children()
 
     def __call__(self, x):
+        """
+        Simplesmente uma outra maneira de chamar a função predict.
+        """
         return self.predict(x)
 
     def predict(self, x):
-        x = deepcopy(x)
+        """
+        Se o nó for folha, retorna a predição referente a si mesmo. Caso
+        contrário, retorna a predição do nó filho referente ao valor da
+        feature a ser informada.
+        """
         if self.is_leaf:
             return self.leaf_value
         else:
+            x = deepcopy(x)
             deciding_col_value = x[self.deciding_col]
             del x[self.deciding_col]
 
             if deciding_col_value in self.children.keys():
                 return self.children[deciding_col_value].predict(x)
             else:
-                return self.children[random.choice(list(self.children.keys()))].predict(x)
+                random_choice = random.choice(list(self.children.keys()))
+                return self.children[random_choice].predict(x)
 
     def most_common_y(self):
+        """
+        Retorna o valor mais comum das labels deste nó, utilizado na
+        função de decidir o valor dos nós folhas.
+        """
         count_dict = dict()
         for y_i in self.y:
             if y_i not in count_dict.keys():
@@ -186,78 +251,11 @@ class Node:
                 biggest_count = count
                 most_common_y = y
         return most_common_y, biggest_count
-
-    def __get_column_values(self, col):
-        assert col >= 0 and col < len(self.X[0]), 'Invalid key value'
-        return [self.X[i][col] for i in range(len(self.X))]
-
-    def __decide_if_leaf(self):
-        if len(self.y) == 0:
-            self.leaf_value, self.leaf_counter = self.parent.most_common_y()
-            return True
-        elif all([i == self.y[0] for i in self.y]):
-            self.leaf_value = self.y[0]
-            self.leaf_counter = len(self.y)
-            return True
-        elif self.X_shape[1] == 0:
-            self.leaf_value, self.leaf_counter = self.parent.most_common_y()
-            return True
-        
-        return False
-
-    def __attribute_entropy(self, col):
-        value_counter = dict()
-        col_values = self.__get_column_values(col)
-        for v in col_values:
-            if v not in value_counter.keys():
-                value_counter[v] = 1
-            else:
-                value_counter[v] += 1
-
-        col_size = len(col_values)
-        for k in value_counter.keys():
-            value_counter[k] /= col_size
-
-        return sum([(-1) * p * np.log2(p) for _, p in value_counter.items()])
-
-    def __decide_most_important_attribute(self):
-        assert self.X_shape[0] > 0
-        
-        highest_entropy = self.__attribute_entropy(0)
-        highest_entropy_col = 0
-        for j in range(1, self.X_shape[1]):
-            tmp_entropy = self.__attribute_entropy(j)
-            if tmp_entropy > highest_entropy:
-                highest_entropy = tmp_entropy
-                highest_entropy_col = j
-        
-        self.deciding_col = highest_entropy_col
-
-    def __get_dropped_col_dataset(self, X, col):
-        for x in X:
-            del x[col]
-        return X
-
-    def __split_dataset_by_classes(self, X, y, deciding_col):
-        classes = set(self.__get_column_values(deciding_col))
-        class_to_dataset = dict()
-        for c in classes:
-            new_X, new_y = zip(*[(x, y_i,) for x, y_i in zip(X, y) if x[deciding_col] == c])
-            class_to_dataset[c] = (new_X, new_y,)
-        return class_to_dataset
-
-    def __generate_children(self):
-        self.__decide_most_important_attribute()
-        self.children = dict()
-
-        new_col_names = [c for i, c in enumerate(self.col_names) if i != self.deciding_col]
-
-        class_to_dataset = self.__split_dataset_by_classes(self.X, self.y, self.deciding_col)
-        for k, (new_X, new_y) in class_to_dataset.items():
-            new_X = self.__get_dropped_col_dataset(new_X, self.deciding_col)
-            self.children[k] = Node(new_X, new_y, new_col_names, self.depth+1, self)
-
+    
     def print(self, inverser):
+        """
+        Função recursiva para imprimir uma visualização da decision tree.
+        """
         if self.is_leaf:
             print(Fore.CYAN + f'{inverser[self.leaf_value]} ({self.leaf_counter})')
             return
@@ -273,27 +271,138 @@ class Node:
             print(Fore.MAGENTA + f'{tabs}    {k}:', end=('\n' if not child.is_leaf else ' '))
             child.print(inverser)
 
+    def __get_column_values(self, col):
+        """
+        Retorna um vetor com os valores de uma coluna.
+        """
+        assert col >= 0 and col < len(self.X[0]), 'Invalid key value'
+        return [self.X[i][col] for i in range(len(self.X))]
+
+    def __decide_if_leaf(self):
+        """
+        Indentifica se o nó deve ser uma folha, e caso sim, define o valor e
+        a o tamanho dos dados que a definiram.
+        """
+        if len(self.y) == 0:
+            self.leaf_value, self.leaf_counter = self.parent.most_common_y()
+            return True
+        elif all([i == self.y[0] for i in self.y]):
+            self.leaf_value = self.y[0]
+            self.leaf_counter = len(self.y)
+            return True
+        elif self.X_shape[1] == 0:
+            self.leaf_value, self.leaf_counter = self.parent.most_common_y()
+            return True
+        
+        return False
+
+    def __attribute_entropy(self, col):
+        """
+        Retorna o valor da entropia de uma certa coluna.
+        """
+        value_counter = dict()
+        col_values = self.__get_column_values(col)
+        for v in col_values:
+            if v not in value_counter.keys():
+                value_counter[v] = 1
+            else:
+                value_counter[v] += 1
+
+        col_size = len(col_values)
+        for k in value_counter.keys():
+            value_counter[k] /= col_size
+
+        return sum([(-1) * p * np.log2(p) for _, p in value_counter.items()])
+
+    def __decide_most_important_attribute(self):
+        """
+        Dado as colunas disponíveis, decide qual delas tem o maior valor
+        de entropia.
+        """
+        assert self.X_shape[0] > 0
+        
+        highest_entropy = self.__attribute_entropy(0)
+        highest_entropy_col = 0
+        for j in range(1, self.X_shape[1]):
+            tmp_entropy = self.__attribute_entropy(j)
+            if tmp_entropy > highest_entropy:
+                highest_entropy = tmp_entropy
+                highest_entropy_col = j
+        
+        self.deciding_col = highest_entropy_col
+
+    def __get_dropped_col_dataset(self, X, col):
+        """
+        Retorna um dataset sem a coluna informada.
+        """
+        for x in X:
+            del x[col]
+        return X
+
+    def __split_dataset_by_classes(self, X, y, deciding_col):
+        """
+        Dada uma coluna, cria um dicionário com os novos datasets referentes
+        às classes de nesta coluna
+        """
+        classes = set(self.__get_column_values(deciding_col))
+        class_to_dataset = dict()
+        for c in classes:
+            new_X, new_y = zip(*[(x, y_i,) for x, y_i in zip(X, y) if x[deciding_col] == c])
+            class_to_dataset[c] = (new_X, new_y,)
+        return class_to_dataset
+
+    def __generate_children(self):
+        """
+        Escolhe o atributo que decide a predição dessa classe e recursivamente
+        cria seus nós 'filhos'
+        """
+        self.__decide_most_important_attribute()
+        self.children = dict()
+
+        new_col_names = [c for i, c in enumerate(self.col_names) if i != self.deciding_col]
+
+        class_to_dataset = self.__split_dataset_by_classes(self.X, self.y, self.deciding_col)
+        for k, (new_X, new_y) in class_to_dataset.items():
+            new_X = self.__get_dropped_col_dataset(new_X, self.deciding_col)
+            self.children[k] = Node(new_X, new_y, new_col_names, self.depth+1, self)
+
 
 class DecisionTree:
 
     def __init__(self, X, y, columns, inverse_class_dict):
+        """
+        Inicializaçao da DT com o dict de inversão da classe das labels
+        fatorizadas para seu nome.
+        """
         self.inverse_class_dict = inverse_class_dict
         self.root = Node(X, y, columns)
 
     def __call__(self, x):
+        """
+        Simplesmente outra maneira de chamar a função predict.
+        """
         return self.predict(x)
 
     def predict(self, x):
+        """
+        Dado um vetor de valores, retorna classe predita para ele.
+        """
         prediction = self.root(x)
         return self.inverse_class_dict[prediction]
     
     def evaluate(self, X, y):
+        """
+        Retorna a porcentagem dos dados classificados corretamente.
+        """
         res = 0
         for x, y_i in zip(X, y):
             res += int(self(x) == self.inverse_class_dict[y_i])
         return res / len(y)
 
     def print(self):
+        """
+        Função que imprime uma visualização da decision tree.
+        """
         self.root.print(self.inverse_class_dict)
         print(Style.RESET_ALL)
         
